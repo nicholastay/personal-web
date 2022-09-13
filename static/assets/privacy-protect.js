9e237bb9bdb2504777b7162ee280a5c59857c1b3
Nicholas Tay <nick@windblume.net>
2022-09-13 12:13:19 +1000

    Privacy protection for JS injections PoC
    It isn't completely foolproof, but an interesting idea. Not sure if it
    will mess with browser addons doing stuff, but will include it as a
    test.
    
    https://krausefx.com/blog/announcing-inappbrowsercom-see-what-javascript-commands-get-executed-in-an-in-app-browser
    
    Probably should add session storage for this one so it doesn't keep
    popping either.

---
+++ static/assets/privacy-protect.js
@@ -0,0 +1,32 @@
+// For context, see:
+// https://krausefx.com/blog/announcing-inappbrowsercom-see-what-javascript-commands-get-executed-in-an-in-app-browser
+
+var protectDisarm = false;
+var protectWarned = false;
+
+function protectWarn() {
+	if (protectWarned)
+		return;
+	var hl = document.getElementById("head-line");
+	hl.innerHTML = "<div class='page'><b>Privacy Warning</b><p>The browser you are using may be unsafe - unintended JavaScript modifications have been detected and blocked.</p><p>It is likely that you are browsing from a mobile social media client, such as Facebook. I recommend that you 'open in an external browser', such as Safari to avoid tracking.</p><p>For more context, please see my blog post about iOS web view tracking. Stay safe!</p></div>";
+	hl.style.backgroundColor = "#fcffa6";
+	hl.style.height = "auto";
+	hl.style.padding = "5px";
+	protectWarned = true;
+}
+
+// Ensure random scripts aren't added.
+var origCE = document.createElement;
+document.createElement = function() {
+	if (!protectDisarm && arguments[0] == "script")
+		protectWarn();
+	else
+		return origCE.apply(this, arguments);
+}
+
+// TODO: Find a better way.
+window._NTProtectDisarm = function() { protectDisarm = true; }
+
+// Ensure random event listeners aren't added.
+// (complete block since we don't use)
+document.addEventListener = protectWarn;
